5556f09ea3cb3b12426a9096c3f30b4e194e0b6b
Cobicobe <jdanforthco3@gmail.com>
2025-06-18 01:16:38 -0500

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ResumeBuilderPro from './app';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ResumeBuilderPro />
+  </React.StrictMode>
+);
